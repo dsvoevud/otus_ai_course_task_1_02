a1d6922f4db20f4d864fa77fb2bc15a03c481db6
Dmitriy Voevudskiy <dimavoevud@gmail.com>
2026-02-05 01:13:56 +0300

    Добавил промты и класс magic, который не должен изменяться.

---
+++ src/magic.py
@@ -0,0 +1,20 @@
+"""Magic class with unused methods."""
+
+
+class Magic:
+    """A class containing magic methods that are not used in the application."""
+
+    @staticmethod
+    def MagicMethod1():
+        """First magic method."""
+        pass
+
+    @staticmethod
+    def MagicMethod2():
+        """Second magic method."""
+        pass
+
+    @staticmethod
+    def MagicMethod3():
+        """Third magic method."""
+        pass
